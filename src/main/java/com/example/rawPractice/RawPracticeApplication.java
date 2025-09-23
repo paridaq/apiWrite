beda4441562e76b2e083f4b07e04ba729da34b69
Biswajit <paridabiswa2k@gmail.com>
2025-09-23 20:47:59 +0530

    first commit

---
+++ src/main/java/com/example/rawPractice/RawPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rawPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RawPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rawPractice/RawPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rawPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RawPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
